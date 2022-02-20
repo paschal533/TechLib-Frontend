404ed3ba3ec7371dcf1015f580cb1755f3bf0fde
unknown <okwuosahpaschal@gmail.com>
2022-02-20 23:17:35 +0100

    third commit

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
-import { MdDownloadForOffline } from 'react-icons/md';
 import { AiTwotoneDelete } from 'react-icons/ai';
 import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
 
